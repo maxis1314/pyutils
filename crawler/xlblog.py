048cf727034a6bbcc2a2d4c177f7487863e111b6
maxis1314 <maxis1314@163.com>
2017-05-27 15:51:37 +0800

    _1

--- crawler/xlblog.py
+++ crawler/xlblog.py
@@ -8,6 +8,8 @@ import sqlite3
 import logutils
 import urllib
 import sys
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 class logger:
     def __init__(self, name):
@@ -32,14 +34,14 @@ class logger:
 
 qinglog = logger('qingblog').get_logger()
 #设置一下主页链接
-oz_url = 'http://wp.rabbit-e.com/wp'
+oz_url = 'http://blog.sina.com.cn/blog'
 conn = sqlite3.connect('qingblog.db')
 cu=conn.cursor()
 
 
 #init table 
 cursor = conn.cursor()        
-cursor.execute("create table if not exists oz(id integer primary key,dt varchar(10),title varchar(100), body text NULL)")
+cursor.execute("create table if not exists oz(id integer primary key,link varchar(100),dt varchar(10),title varchar(100), body text NULL)")
 conn.commit()
 cursor.close()
 
@@ -69,13 +71,13 @@ def get_html(url):
         header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; rv:35.0) Gecko/20100101 Firefox/35.0',\
                 'Accept': 'text/plain,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',\
                 'Accept-Language': 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',\
-                'Accept-Encoding': 'gzip, deflate',\
+                #'Accept-Encoding': 'gzip, deflate',\
                 'Connection': 'keep-alive'}
         request = urllib2.Request(url, None, header)
         response = urllib2.urlopen(request)
         result = response.read()
         return result;
-        #return gz_decode(result).decode('UTF-8')
+        #return gz_decode(result).decode('GBK')#.decode('UTF-8')
     except Exception,e:
         print qinglog.error('get_page failed, url:%s, reason:%s' % (url, str(e)))
         return None
@@ -84,13 +86,9 @@ def extract(page):
     if page is None:
         return (None,None,None)
 
-    linkpattern = re.compile(u'<a class="more" href="(.*?)">查看全文>></a>', re.S | re.U)
-    links = linkpattern.findall(page)
-    yearpattern = re.compile(u'<strong>(\d{4,4})</strong>', re.S | re.U)
-    years = yearpattern.findall(page)
-    datepattern = re.compile(u'<em>(\d{1,2}/\d{1,2})</em>', re.S | re.U)
-    dates = datepattern.findall(page)
-    return (links, years, dates)
+    linkpattern = re.compile(u'class="postTitle2" href="(.*?)">', re.S | re.U)
+    links = linkpattern.findall(page)    
+    return (links,links,links)
 
 def sub_div(m):
     content = ''
@@ -108,48 +106,28 @@ def process_article(article):
     body = ''
     img_urls = []
     
-    titlepattern = re.compile(u'<span class="title">(.*?)</span>', re.S | re.U)
+    titlepattern = re.compile(u'<a id="cb_post_title_url" class="postTitle2" href=".*?">(.*?)</a>', re.S | re.U)
     result = titlepattern.search(article)
     if result is not None:
         title = result.group(1).strip()
-
-    bodypattern = re.compile(u'<div class="caption">(.*?)<!--feed描述 start-->', re.S | re.U)
+    
+    bodypattern = re.compile(u'<div id="cnblogs_post_body">(.*?)</div><div id="MySignature">', re.S | re.U)
     result = bodypattern.search(article)
     if result is not None:
         body = result.group(1)
 
-    imgpattern = re.compile(u'<img (.*?)/>', re.S | re.U | re.I)
-    imglist = imgpattern.findall(body)
-    for img in imglist:
-        s = re.search(u'real_src="(.*?)"', img, flags = re.S | re.U | re.I)
-        if s is not None:
-            img_urls.append(s.group(1).strip())
-        else:
-            s = re.search(u'src="(.*?)"', img, flags = re.S | re.U | re.I)
-            if s is not None:
-                img_urls.append(s.group(1).strip())
-    divpattern = re.compile(u'<div[\S ]*?>.*?</div>', re.S | re.I | re.U)
-    body = divpattern.sub(sub_div, body)
-    body = re.sub(u'<div>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'</div>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'<p>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'<p class="blockquote_name">.*?</p>', '', body, flags = re.I | re.S | re.U)
-    
-    #body = re.sub(u'<a.*?>.*?</a>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'<blockquote>.*?</blockquote>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'</p>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'&nbsp;', ' ', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'<br.*?/>', '', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'\s{4,}', '\n', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'&lt;', '<', body, flags = re.I | re.S | re.U)
-    body = re.sub(u'&gt;', '>', body, flags = re.I | re.S | re.U)
-    return (title, body.strip(), img_urls)
+    imgpattern = re.compile(u'<span id="post-date">(.*?)</span>', re.S | re.U | re.I)
+    result = imgpattern.search(article)
+    if result is not None:
+        post_date = result.group(1)
 
-def insert_db(year, date, link, title, body, img_path):
-    full_date = '%s/%s' % (year, date)
-    full_date = full_date.replace('/','')
+    return (title, body.strip(), post_date)
+
+def insert_db(dt,link, title, body):
+    title = title.replace("\r", u'')
+    body = body.replace("\n", u'')
     insert = u'insert into oz(dt'
-    values = u"values(" + full_date
+    values = u"values(" + u"'" + dt + u"'"
     if link is not None and len(link) > 0:
         insert = insert + u', link'
         values = values + u", '" + link + u"'"
@@ -158,12 +136,9 @@ def insert_db(year, date, link, title, body, img_path):
         values = values + u", '" + title + u"'"
     if body is not None and len(body) > 0:
         insert = insert + u', body'
-        values = values + u", '" + body + u"'"
-    if img_path is not None and len(img_path) > 0:
-        insert = insert + u', img'
-        values = values + u", '" + ','.join(img_path) + u"'"
+        values = values + u", '" + body + u"'"    
     sql = insert + u')' + values + u')'
-    #qinglog.info(sql)
+    #qinglog.info(sql)    
     try:
         cursor = conn.cursor()
         cursor.execute(sql)
@@ -186,30 +161,30 @@ def clear_oz():
         return False
 
 if __name__ == '__main__':
-    print get_html('http://www.shanghaidaily.com/')
-    sys.exit()
+    #print get_html('http://www.shanghaidaily.com/')
+    #sys.exit()
     clear_oz()
     page = 1
+    oz_url='http://www.cnblogs.com/bluescorpio/default.html?page='
     while True:
     #while page == 1:
-        page_url = '%s/page/%s' % (oz_url, str(page))
+        page_url = '%s%s' % (oz_url, str(page))
         qinglog.info('get page:%s' % page_url)
         page_html = get_html(page_url)
-        qinglog.info(page_html)
+        
         if page_html is None:
             qinglog.info('%s is None' % page_url)
             page = page + 1
-            continue
+            continue       
         
         links,years,dates = extract(page_html)
-        if links is None or years is None or dates is None:
-            qinglog.info('no links or years or dates in %s' % page_url)
-            break
-        if len(links) == 0 or len(years) == 0 or len(dates) == 0:
-            qinglog.info('no links or years or dates in %s' % page_url)
-            break
         
-        if len(links) == len(years) and len(links) == len(dates):
+        print links;
+        if links is None or len(links) == 0:
+            qinglog.info('no links or years or dates in %s' % page_url)
+            break       
+        
+        if True:
             for link,year,date in zip(links,years,dates):
                 #article_url = '%s/%s.html' % (oz_url, link)
                 article_url = link
@@ -219,11 +194,9 @@ if __name__ == '__main__':
                     page = page + 1
                     continue
 
-                title, body, img_urls = process_article(article_page)
-                img_path = []
-                get_img(img_urls, img_path)
+                title, body, dt = process_article(article_page)                                
                 title = title.replace('&amp;', '&')
-                insert_db(year, date, article_url, title.replace("'", "''"), body.replace("'", "''"), img_path)                            
+                insert_db(dt,article_url, title.replace("'", "''"), body.replace("'", "''"))                            
         else:
             qinglog.info('length of years, dates, links not equal in %s' % page_url)
         page = page + 1
