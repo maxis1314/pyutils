218b0364bd7b5b1176d925387c8715b6174be543
maxis1314 <maxis1314@163.com>
2017-06-05 16:18:50 +0800

    add body search 2017/06/05

--- web/app.py
+++ web/app.py
@@ -36,7 +36,7 @@ def blogSearch():
         _from=0
     else:
         _from=int(float(_from))
-    list = mysql.query(u'select * from blog where title like "%'+_key+u'%" order by id limit '+str(_from)+u',100')
+    list = mysql.query(u'select * from blog where title like "%'+_key+u'%" or body like "%'+_key+u'%" order by id limit '+str(_from)+u',100')
     return render_template('blog_index.html',list=list,_from=_from)
     
 @app.route('/showSignUp')
