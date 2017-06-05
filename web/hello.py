67cb635b1e47aa33d30323949bc06716d984426d
maxis1314 <maxis1314@163.com>
2017-06-05 14:09:34 +0800

    2_2017/06/05

--- web/hello.py
+++ web/hello.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route("/home")
+def home():
+    return render_template('index.html')
+@app.route("/gaga2")
+def hello23():
+    return "gaga2 World!"    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
