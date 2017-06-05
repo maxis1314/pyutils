4a4e153c7eb463a9f8ed219d8ba50b379b9955ca
maxis1314 <maxis1314@163.com>
2017-06-05 16:17:44 +0800

    add search function 2017/06/05

--- web/app.py
+++ web/app.py
@@ -16,7 +16,7 @@ def main():
 def blogDetail():
     _id = request.args.get('id')
     blog = mysql.query('select * from blog where id=%s',(_id))
-    return render_template('blog_detail.html',blog=blog[0])
+    return render_template('blog_detail.html',blog=blog[0],id=int(_id))
 
 @app.route('/blogHome')
 def blogHome():
@@ -26,6 +26,17 @@ def blogHome():
     else:
         _from=int(float(_from))
     list = mysql.query(u'select * from blog order by id limit '+str(_from)+u',20')
+    return render_template('blog_index.html',list=list,_from=_from)
+
+@app.route('/blogSearch')
+def blogSearch():
+    _from = request.args.get('from')
+    _key = request.args.get('key')
+    if _from is None or _from.strip()== '':
+        _from=0
+    else:
+        _from=int(float(_from))
+    list = mysql.query(u'select * from blog where title like "%'+_key+u'%" order by id limit '+str(_from)+u',100')
     return render_template('blog_index.html',list=list,_from=_from)
     
 @app.route('/showSignUp')
