132432955e195a238052ee7bd1eebb67437029e6
maxis1314 <maxis1314@163.com>
2017-06-06 13:28:40 +0800

    1 2017/06/06

--- crawler/batch.py
+++ crawler/batch.py
@@ -4,10 +4,12 @@ import subprocess
     
 if __name__ == '__main__':   
     db = MysqlBase('python')
-    list = db.query('select * from bloglist')
+    list = db.query('select * from bloglist where flag=0')
     
-    for i in list:
-        subprocess.Popen(u'python cnblog.py '+i[2],shell=True)
-    
+    for i in list:        
+        subprocess.Popen(u'python cnblog.py '+i[2],shell=True).wait()
+        db.execute('update  bloglist set flag = 1 where id =%d' % (i[0]));
+        
+    #print 22222222222
     #db.multi_insert_test()
     #db.execute('create table blog(link varchar(20),dt varchar(20),title varchar(200),body text)');
