65ba5ac1bcd9d506d17261c692f16b57f00094e0
maxis1314 <maxis1314@163.com>
2017-06-16 10:38:08 +0800

    1 2017/06/16

--- web/views/rss.py
+++ web/views/rss.py
@@ -5,7 +5,7 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from flask import render_template
-from flask import flash,session
+from flask import flash,session,json
 from tools.MysqlBase import *
 import hashlib
 import feedparser
@@ -18,9 +18,18 @@ rss_view = Blueprint('rss', __name__)
 @rss_view.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':       
-        feed = feedparser.parse('http://feed.cnblogs.com/blog/u/161528/rss')        
-        return render_template('rss/index.html',feeds = feed['items'],active='rss')
+        feeds = mysql.query_h('select * from rss order by dt desc')     
+        return render_template('rss/index.html',feeds = feeds,active='rss')
         
     if request.method == 'POST':        
         return redirect(url_for('todos.show'))
-
+        
+@rss_view.route('/sync')
+def sync():
+    mysql.execute('delete from rss')
+    feeds = mysql.query_h('select * from feed')     
+    for url in feeds:
+        feed = feedparser.parse(url['url']) 
+        for item in feed['items']:
+            mysql.insert('insert ignore into rss(dt,link,title,body) values(%s,%s,%s,%s)',(item.get('date'),item.get('link'), item.get('title'), item.get('summary')))
+    return redirect(url_for('rss.index'))
