2d020cd974b3ccaf7701102fcc7f2fa87c7f1cc6
maxis1314 <maxis1314@163.com>
2017-06-21 16:59:40 +0800

    4 2017/06/21

--- web/apps/__init__.py
+++ web/apps/__init__.py
@@ -2,3 +2,6 @@
 from flask import Flask, jsonify, abort, make_response, render_template
 app = Flask(__name__)
 app.config.from_object("config")
+
+app.template_folder = "../templates"
+app.static_folder = "../static"
