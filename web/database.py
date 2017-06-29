8ddc6cb5ea21bbe525015b9412b1406c30f64eda
maxis1314 <maxis1314@163.com>
2017-06-29 11:42:02 +0800

    5 2017/06/29

--- web/database.py
+++ web/database.py
@@ -6,9 +6,48 @@ import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session,sessionmaker
 from sqlalchemy.pool import NullPool
+from sqlalchemy import event
+from sqlalchemy import exc
+from sqlalchemy import select
 
 config = config.rec()
-engine = sa.create_engine(config.database + '?charset=utf8',poolclass=NullPool)
+engine = sa.create_engine(config.database + '?charset=utf8',  pool_recycle=3600)
+
+
+@event.listens_for(engine, "engine_connect")
+def ping_connection(connection, branch):
+    if branch:
+        # "branch" refers to a sub-connection of a connection,
+        # we don't want to bother pinging on these.
+        return
+
+    # turn off "close with result".  This flag is only used with
+    # "connectionless" execution, otherwise will be False in any case
+    save_should_close_with_result = connection.should_close_with_result
+    connection.should_close_with_result = False
+
+    try:
+        # run a SELECT 1.   use a core select() so that
+        # the SELECT of a scalar value without a table is
+        # appropriately formatted for the backend
+        connection.scalar(select([1]))
+    except exc.DBAPIError as err:
+        # catch SQLAlchemy's DBAPIError, which is a wrapper
+        # for the DBAPI's exception.  It includes a .connection_invalidated
+        # attribute which specifies if this connection is a "disconnect"
+        # condition, which is based on inspection of the original exception
+        # by the dialect in use.
+        if err.connection_invalidated:
+            # run the same SELECT again - the connection will re-validate
+            # itself and establish a new connection.  The disconnect detection
+            # here also causes the whole connection pool to be invalidated
+            # so that all stale connections are discarded.
+            connection.scalar(select([1]))
+        else:
+            raise
+    finally:
+        # restore "close with result"
+        connection.should_close_with_result = save_should_close_with_result
 
 db_session = scoped_session(sessionmaker(bind=engine))
 
